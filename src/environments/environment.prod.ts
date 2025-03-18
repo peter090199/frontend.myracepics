27bdae7fd23a1a7e0d7484331bf4037575689f02
Peds Yorpo <pedroyorpo17@gmail.com>
2025-03-18 21:37:49 +0800

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
    // apiUrl: 'http://localhost:8000/api/'
-    apiUrl: 'https://red-anteater-382469.hostingersite.com/public/api/'
-    //  apiUrl: 'https://aliceblue-barracuda-249949.hostingersite.com/public/api/'
+    //apiUrl: 'https://red-anteater-382469.hostingersite.com/public/api/'
+    apiUrl: 'https://lightgreen-pigeon-122992.hostingersite.com/public/api/'
+
 };
